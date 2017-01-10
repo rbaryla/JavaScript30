259fb7a9c46d7b3001f04a594d424991f8b0d81b
rbaryla <robert@em4u.pl>
2017-01-11 00:25:07 +0100

    Added sounds

---
+++ 03 - CSS Variables/js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by rbaryla on 10.01.2017.
+ */
+
+const controls = document.querySelectorAll('.controls input');
+
+controls.forEach((element) => {
+    element.addEventListener('change', changeHandeler);
+    element.addEventListener('input', changeHandeler);
+});
+
+function changeHandeler() {
+    const sufix = this.dataset['sizing'] || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + sufix);
+}
