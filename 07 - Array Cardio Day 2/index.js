888a751786753693d7a15d126e0e22f0d3314fda
rbaryla <robert@em4u.pl>
2017-01-15 04:50:32 +0100

    JavaScript30-4 07 - Array Cardio Day 2

---
+++ 07 - Array Cardio Day 2/index.js
@@ -0,0 +1,57 @@
+/**
+ * Created by rbaryla on 15.01.2017.
+ */
+    // ## Array Cardio Day 2
+
+const people = [
+        { name: 'Wes', year: 1988 },
+        { name: 'Kait', year: 1986 },
+        { name: 'Irv', year: 1970 },
+        { name: 'Lux', year: 2015 }
+    ];
+
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+function isPersonAdult(person) {
+    return (new Date).getFullYear() - person.year >= 19;
+}
+
+const isAdult = people.some(isPersonAdult);
+
+console.log({isAdult});
+
+// Array.prototype.every() // is everyone 19 or older?
+const areEveryAdult = people.every(isPersonAdult);
+
+console.log({areEveryAdult});
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+ function findIdOfObject (id, obj) {
+    return obj => obj.id === id;
+}
+
+const findCommentId = findIdOfObject(823423);
+
+const comment = comments.find(findCommentId);
+
+console.log({comment});
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+const index = comments.findIndex(findCommentId);
+
+console.log({index});
+comments.splice(index, 1);
+
+console.log(comments);
